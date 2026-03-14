4ff290bce4099e6e6a2ab880ac9393b4e798d757
Abhishek Ruhela <94753084+mysterioxx@users.noreply.github.com>
2026-03-14 22:31:08 +0530

    Initial Commit

---
+++ EarSafe/EarSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarSafeApp.swift
+//  EarSafe
+//
+//  Created by Abhishek Ruhela on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct EarSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
